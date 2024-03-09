179d18fbc3a107dc49aa40c14684758060fcaaff
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-09 07:02:59 +0100

    Get Bean from IoC - V1

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -2,15 +2,25 @@ package com.dgmf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// SpringApplication.run(Application.class, args);
+		// HelloWorld helloWorld = new HelloWorld();
 
-		HelloWorld helloWorld = new HelloWorld();
+		// Get Bean from IoC - V1
+		// ConfigurableApplicationContext
+		var context = SpringApplication.run(Application.class, args);
+		HelloWorld helloWorld = context.getBean(HelloWorld.class);
 		System.out.println(helloWorld.sayHello());
+	}
+
+	@Bean
+	public HelloWorld helloWorld() {
+		return new HelloWorld();
 	}
 
 }
