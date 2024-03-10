9fd2f76996a4a5319899f90375285b4e05b5fc52
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-10 03:18:08 +0100

    Get Bean from IoC - @Component Annotation

--- src/main/java/com/dgmf/HelloWorld.java
+++ src/main/java/com/dgmf/HelloWorld.java
@@ -1,5 +1,8 @@
 package com.dgmf;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloWorld {
     public String sayHello() {
         return "Hello Spring People !";
