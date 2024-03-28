20b12e28dab2389df59dbf12c04866e390cffd81
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-28 06:55:55 +0100

    @PostMapping("/posts") ==> Send Text in Request Body

--- src/main/java/com/dgmf/a/restapi/controller/FirstController.java
+++ src/main/java/com/dgmf/a/restapi/controller/FirstController.java
@@ -1,14 +1,17 @@
 package com.dgmf.a.restapi.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello from FirstController";
+    }
+
+    @PostMapping("/posts")
+    public String post(@RequestBody String message) {
+        return "Request Accepted and Message is : " + message;
     }
 }
